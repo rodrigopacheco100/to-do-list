503ff9e94860eeec4730e3a46c213650f140a723
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-26 13:49:59 -0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import "./database/connection";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3333, () => {
+   console.log("Server is running!");
+});
